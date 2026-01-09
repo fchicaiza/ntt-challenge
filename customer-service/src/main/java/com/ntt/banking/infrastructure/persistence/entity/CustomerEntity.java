4ae66d56129d992d6ca96ba1d766c4738cd9eb62
Fernando Chicaiza <fchicaiza.g1990@gmail.com>
2026-01-09 00:57:52 +0000

    feat: implement Person and Customer entities with JPA inheritance

--- customer-service/src/main/java/com/ntt/banking/infrastructure/persistence/entity/CustomerEntity.java
+++ customer-service/src/main/java/com/ntt/banking/infrastructure/persistence/entity/CustomerEntity.java
@@ -1,15 +1,26 @@
 package com.ntt.banking.infrastructure.persistence.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 import java.util.UUID;
 
 @Entity
 @Table(name = "customers")
-public class CustomerEntity {
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerEntity extends PersonEntity {
 
     @Id
-    private UUID id;
+    @Column(name = "customer_id")
+    private UUID customerId;
 
     @Column(nullable = false)
-    private String name;
+    private String password;
+
+    @Column(nullable = false)
+    private Boolean status;
 }
---
+++ customer-service/src/main/java/com/ntt/banking/infrastructure/persistence/entity/PersonEntity.java
@@ -0,0 +1,33 @@
+package com.ntt.banking.infrastructure.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@MappedSuperclass
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class PersonEntity {
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String gender;
+
+    @Column(nullable = false, unique = true)
+    private String identification;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String phone;
+}
