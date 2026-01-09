a49fdea892058d20f79cee8f50ebfe9b19342bc9
Fernando Chicaiza <fchicaiza.g1990@gmail.com>
2026-01-09 00:41:57 +0000

    refactor: remove legacy banking-api monolith artifacts

--- banking-api/src/main/java/com/ntt/banking/application/exception/CustomerAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.ntt.banking.application.exception;
-
-public class CustomerAlreadyExistsException extends RuntimeException {
-
-    public CustomerAlreadyExistsException(String identification) {
-        super("Customer already exists with identification: " + identification);
-    }
-}
